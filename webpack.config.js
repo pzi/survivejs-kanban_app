6dad8c0d1e711d14dc85c3934940e2eae9207628
Patrik Affentranger <patrik.affentranger@gmail.com>
2015-09-26 14:44:44 +0800

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var ROOT_PATH = path.resolve(__dirname);
@@ -9,7 +10,15 @@ module.exports = {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
   },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    port: 8080,
+    progress: true
+  },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       title: 'Kanban app'
     })
