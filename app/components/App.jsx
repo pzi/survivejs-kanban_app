188cd9005db3794ad544ad7b31e58aba946b6464
Patrik Affentranger <patrik.affentranger@gmail.com>
2015-09-27 16:06:04 +0800

    Move notes into state and add new note button

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,23 +2,45 @@ import uuid from 'node-uuid';
 import React from 'react';
 import Notes from 'components/Notes.jsx';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn Webpack'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  }
-];
-
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+    this.addNote = this.addNote.bind(this);
+  }
+
+  addNote() {
+    this.setState({
+      notes: this.state.notes.concat({
+        id: uuid.v4(),
+        task: 'New Task'
+      })
+    });
+  }
+
   render() {
-    return <Notes items={notes} />;
+    const notes = this.state.notes;
+
+    return (
+      <div>
+        <button className='add-note' onClick={this.addNote}>+</button>
+        <Notes items={notes} />
+      </div>
+    );
   }
 }
