0182a5f8fe5751e3d07dddf596828934afce9b44
Patrik Affentranger <patrik.affentranger@gmail.com>
2015-09-27 15:26:50 +0800

    Create simple notes and display them with React

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,36 @@
+import uuid from 'node-uuid';
 import React from 'react';
 import Note from 'components/Note.jsx';
 
+const notes = [
+  {
+    id: uuid.v4(),
+    task: 'Learn Webpack'
+  },
+  {
+    id: uuid.v4(),
+    task: 'Learn React'
+  },
+  {
+    id: uuid.v4(),
+    task: 'Do laundry'
+  }
+];
+
 export default class App extends React.Component {
+  renderNote(note) {
+    return (
+      <li key={`note${note.id}`}>
+        <Note task={note.task} />
+      </li>
+    );
+  }
+
   render() {
-    return <Note />;
+    return (
+      <ul>
+        {notes.map(this.renderNote)}
+      </ul>
+    );
   }
 }
