2798746e7198d85b78a596a8311a0d0e7e6d65e9
Patrik Affentranger <patrik.affentranger@gmail.com>
2015-09-27 16:29:13 +0800

    Add editing functionality to Note

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,7 +1,61 @@
 import React from 'react';
 
 export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editing: false
+    };
+
+    this.edit = this.edit.bind(this);
+    this.checkEnter = this.checkEnter.bind(this);
+    this.finishEdit = this.finishEdit.bind(this);
+  }
+
+  edit() {
+    this.setState({
+      editing: true
+    });
+  }
+
+  finishEdit(event) {
+    this.props.onEdit(event.target.value);
+
+    this.setState({
+      editing: false
+    });
+  }
+
+  checkEnter(event) {
+    if (event.key === 'Enter') {
+      this.finishEdit(event);
+    }
+  }
+
+  renderEdit() {
+    return <input type='text'
+      autoFocus={true}
+      defaultValue={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />;
+  }
+
+  renderTask() {
+    return (
+      <span onClick={this.edit}>
+        {this.props.task}
+      </span>
+    );
+  }
+
   render() {
-    return <div>{this.props.task}</div>;
+    const editing = this.state.editing;
+
+    return (
+      <div>
+        {editing ? this.renderEdit() : this.renderTask()}
+      </div>
+    );
   }
 }
