29a4db319b43e50126ce014d5ef5cb29b5d757d8
Patrik Affentranger <patrik.affentranger@gmail.com>
2015-09-27 21:43:33 +0800

    Move logic related with data to NoteStore

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,78 +1,37 @@
-import 'array.prototype.findindex';
-import uuid from 'node-uuid';
 import React from 'react';
-import Notes from 'components/Notes.jsx';
+import Notes from 'components/Notes';
+import NoteActions from 'actions/NoteActions';
+import NoteStore from 'stores/NoteStore';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn Webpack'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do laundry'
-        }
-      ]
-    };
-    this.addNote = this.addNote.bind(this);
-    this.editNote = this.editNote.bind(this);
-    this.deleteNote = this.deleteNote.bind(this);
+    this.storeChanged = this.storeChanged.bind(this);
+    this.state = NoteStore.getState();
+  }
+
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged);
+  }
+
+  componentWillUnmount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+
+  storeChanged(state) {
+    this.setState(state);
   }
 
   addNote() {
-    this.setState({
-      notes: this.state.notes.concat({
-        id: uuid.v4(),
-        task: 'New Task'
-      })
-    });
+    NoteActions.create({task: 'New task'});
   }
 
-  editNote(noteId, task) {
-    let notes = this.state.notes;
-    const noteIndex = this.findNoteIndex(noteId);
-
-    if (noteIndex < 0) {
-      return;
-    }
-
-    notes[noteIndex].task = task;
-
-    this.setState({notes});
+  editNote(id, task) {
+    NoteActions.update({id, task});
   }
 
-  deleteNote(noteId) {
-    const notes = this.state.notes;
-    const noteIndex = this.findNoteIndex(noteId);
-
-    if (noteIndex < 0) {
-      return;
-    }
-
-    const leftOfDeleted = notes.slice(0, noteIndex);
-    const rightOfDeleted = notes.slice(noteIndex + 1);
-
-    this.setState({
-      notes: leftOfDeleted.concat(rightOfDeleted)
-    });
-  }
-
-  findNoteIndex(noteId) {
-    const notes = this.state.notes;
-    const noteIndex = notes.findIndex((note) => note.id === noteId );
-
-    if (noteIndex < 0) {
-      console.warn('Failed to find note', notes, noteId);
-    }
-    return noteIndex;
+  deleteNote(id) {
+    NoteActions.delete(id);
   }
 
   render() {
---
+++ app/stores/NoteStore.js
@@ -0,0 +1,62 @@
+import 'array.prototype.findindex';
+import uuid from 'node-uuid';
+import alt from 'utils/alt';
+import NoteActions from 'actions/NoteActions';
+
+class NoteStore {
+  constructor() {
+    this.bindActions(NoteActions);
+
+    this.notes = [];
+  }
+
+  create(note) {
+    const notes = this.notes;
+    note.id = uuid.v4();
+    this.setState({
+      notes: notes.concat(note)
+    });
+  }
+
+  update({id, task}) {
+    let notes = this.notes;
+    const noteIndex = this.findNote(id);
+
+    if (noteIndex < 0) {
+      return;
+    }
+
+    notes[noteIndex].task = task;
+
+    this.setState({notes});
+  }
+
+  delete(id) {
+    const notes = this.notes;
+    const noteIndex = this.findNote(id);
+
+    if (noteIndex < 0) {
+      return;
+    }
+
+    const leftOfDeleted = notes.slice(0, noteIndex);
+    const rightOfDeleted = notes.slice(noteIndex + 1);
+
+    this.setState({
+      notes: leftOfDeleted.concat(rightOfDeleted)
+    });
+  }
+
+  findNote(id) {
+    const notes = this.notes;
+    const noteIndex = notes.findIndex((note) => note.id === id);
+
+    if (noteIndex < 0) {
+      console.warn('Failed to find note', notes, id);
+    }
+
+    return noteIndex;
+  }
+}
+
+export default alt.createStore(NoteStore, 'NoteStore');
